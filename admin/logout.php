ae8c0adcce3dc3a44009bca86fc1bc8aa2ca7185
DanteSIT <dantelespoirsit@gmail.com>
2026-08-21 09:14:20 +0400

    Optimize for Lighthouse: project icons, contrast fixes, minified CSS/JS, toning content, WebP thumbnails

--- admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/auth.php';
-require_once __DIR__ . '/functions.php';
-
-auth_logout();
-header('Location: login.php');
-exit;
